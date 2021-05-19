5c694a107704ed5a4c7d3cb063ecbd476fff4f5c
Luciano <lucianoaf-98@hotmail.com.ar>
2021-05-19 15:51:49 -0300

    archivitos

---
+++ PHP/Clases/Semana_12-16_5/JS/script.js
@@ -0,0 +1,17 @@
+console.log('Arrancando con funciones')
+console.log('Procedimientos')
+
+function llamarConsola() {
+    
+    console.log('Estoy en una funcion')
+
+    alert('Otra tarea')
+
+
+}
+
+llamarConsola()
+
+var nombreUsuario = prompt('');
+
+document.write('<h1>' + nombreUsuario + '</h1>');
