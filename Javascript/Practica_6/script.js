8e04c8698e07fcad092936c29166590b7805838c
Luciano <lucianoaf-98@hotmail.com.ar>
2021-05-25 23:22:35 -0300

    tp1_javascript

---
+++ Javascript/Practica_6/script.js
@@ -0,0 +1,12 @@
+function suma(val1, val2) {
+
+    return (parseInt(val1) + parseInt(val2));
+
+}
+
+let primera = 40;
+let segunda = 16;
+let sumatotal = suma(primera,segunda);
+
+document.write(sumatotal);
+
